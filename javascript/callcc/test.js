e1011243103dc4166059e55da0ac44f56f842270
Hsi Fun <manbaum@hotmail.com>
2016-02-24 00:04:36 +0800

    test.js: almost ready to go.

--- javascript/callcc/test.js
+++ javascript/callcc/test.js
@@ -2,12 +2,7 @@
 "use strict"
 
 var u2 = require("uglify-js");
-
-var convert_f2c = _async.convert_f2c;
-var convert_p2c = _async.convert_p2c;
-var convert_f2p = _async.convert_f2p;
-var convert_c2p = _async.convert_c2p;
-var callcc = _async.callcc;
+var callcc = require("./callcc").callcc;
 
 var find_first_function = function(ast) {
 	var found = null;
@@ -134,27 +129,6 @@ var modify_function_body = function(body) {
 	return process_callcc(new_body);
 };
 
-var _test2 = function() {
-	var a = callcc(function(cc) {
-		return callcc(function(cc) {
-			return 5;
-		});
-	}) + callcc(function(cc) {
-		return 5;
-	});
-	print("a = " + a);
-	var b = callcc(function(cc) {
-		cc(null, 6);
-	});
-	print("b = " + b);
-	var k;
-	var x = callcc(function(cc) {
-		k = cc;
-		return 0;
-	});
-	print("x = " + x);
-	if (x < 5) k(null, x + 1);
-};
 
 var testp = function() {
 	let code  = "(" + _test2.toString() + ")";
@@ -171,7 +145,6 @@ var testp = function() {
 	}
 };
 
-testp();
 
 var test2c = function() {
 	let a = null;
@@ -199,3 +172,217 @@ var test2c = function() {
 		});
 	});
 };
+
+var use_callcc = function(f) {
+	let code = "(" + f.toString() + ")";
+	console.log(code);
+	let ast = u2.parse(code);
+	let new_ast = do_rewrite(ast);
+	let new_code = new_ast.print_to_string({ beautify: true });
+	console.log(new_code);
+	return eval(new_code);
+};
+
+var do_rewrite = function(ast) {
+	return ast.transform(new u2.TreeTransformer(function(node) {
+		if (node instanceof u2.AST_Function) {
+			return new u2.AST_Function({
+				name: node.name,
+				argnames: node.argnames,
+				body: rewrite_function_body(node.body)
+			});
+		}
+	}));
+};
+
+var rewrite_function_body = function(body) {
+	let new_body = [];
+	let var_defs = [];
+	for (let i = 0; i < body.length; i++) {
+		let st = body[i];
+		if (st instanceof u2.AST_Var) {
+			st.definitions.forEach(function(def) {
+				var_defs.push(def.name);
+				if (def.value) new_body.push(new u2.AST_Assign({
+					left: def.name,
+					operator: "=",
+					right: def.value
+				}));
+			});
+		} else {
+			new_body.push(st);
+		}
+	}
+	if (var_defs.length) new_body.unshift(new u2.AST_Var({ definitions: var_defs }));
+	return find_and_rewrite_callcc(new_body);
+};
+
+var find_and_rewrite_callcc = function(body) {
+	let new_body = [];
+	for (let i = 0; i < body.length; i++) {
+		let st = body[i];
+		let callccs = find_callcc(st);
+		if (callccs.length) {
+			let rest = body.slice(i + 1);
+			let new_st = rewrite_callcc(st, callccs, rest);
+			new_body.push(new_st);
+			break;
+		} else {
+			new_body.push(st);
+		}
+	}
+	return new_body;
+};
+
+var find_callcc = function(st) {
+	let callccs = [];
+	st.walk(new u2.TreeWalker(function(node) {
+		if (node instanceof u2.AST_Function) {
+			return true;
+		} else if (node instanceof u2.AST_Call) {
+			let name = node.expression.name;
+			if (name == "callcc" || name == "callcc4c") {
+				callccs.push(node);
+			}
+		}
+	}));
+	return callccs;
+};
+
+var rewrite_callcc = function(st, callccs, rest) {
+	let new_st, last, cps;
+	callccs.forEach(function(c, i) {
+		let lambda = new u2.AST_Function({
+			name: c.args[0].name,
+			argnames: c.args[0].argnames,
+			body: rewrite_function_body(c.args[0].body)
+		});
+		let context = c.args[1] ? c.args[1] : new u2.AST_Null();
+		let argnames = [
+			new u2.AST_SymbolVar({ name: "$error" + i + "$" }),
+			new u2.AST_SymbolVar({ name: "$value" + i + "$" })
+		];
+		if (!new_st) {
+			last = cps = rewrite_return_and_throw(new u2.AST_Function({
+				argnames: argnames,
+				body: rewrite_function_body(add_rewrited_statement(rest, st, i))
+			}));
+			new_st = new u2.AST_Call({
+				expression: c.expression,
+				args: [lambda, context, cps]
+			});
+		} else {
+			cps = new u2.AST_Function({
+				argnames: argnames,
+				body: last.body
+			});
+			let var_name = new u2.AST_SymbolVar({ name: "$error" + (i - 1) + "$" });
+			last.body = [
+				new u2.AST_If({
+					condition: var_name,
+					body: new u2.AST_Throw({ value: var_name })
+				}),
+				new u2.AST_Call({
+					expression: c.expression,
+					args: [lambda, context, cps]
+				})
+			];
+			last = cps;
+		}
+	});
+	return new_st;
+};
+
+var add_rewrited_statement = function(body, st, j) {
+	let  i = 0;
+	body.unshift(st.transform(new u2.TreeTransformer(function(node) {
+		if (node instanceof u2.AST_Call) {
+			let name = node.expression.name;
+			if (name == "callcc" || name == "callcc4c") {
+				return new u2.AST_SymbolVar({ name: "$value" + (i++) + "$" });
+			}
+		}
+	})));
+	var var_name = new u2.AST_SymbolVar({ name: "$error" + j + "$" });
+	body.unshift(new u2.AST_If({
+		condition: var_name,
+		body: new u2.AST_Throw({ value: var_name })
+	}));
+	return body;
+};
+
+var rewrite_return_and_throw = function(cc) {
+	return cc.transform(new u2.TreeTransformer(function(node) {
+		if (node != cc && node instanceof u2.AST_Function) {
+			return node;
+		} else if (node instanceof u2.AST_Return) {
+			node.value = new u2.AST_Call({
+				expression: new u2.AST_SymbolVar({ name: "cc" }),
+				args:[
+					new u2.AST_Null(),
+					node.value
+				]
+			});
+			return node;
+		} else if (node instanceof u2.AST_Throw) {
+			return new u2.AST_Return({
+				value: new u2.AST_Call({
+					expression: new u2.AST_SymbolVar({ name: "cc" }),
+					args:[node.value]
+				})
+			});
+		}
+	}));
+};
+
+var _test2 = function() {
+	var a = callcc(function(cc) {
+		return callcc(function(cc) {
+			return 5;
+		});
+	}) + callcc(function(cc) {
+		return 5;
+	});
+	print("a = " + a);
+	var b = callcc(function(cc) {
+		var f = 6;
+		cc(null, f);
+	});
+	print("b = " + b);
+	var k;
+	var x = callcc(function(cc) {
+		k = cc;
+		return 0;
+	});
+	print("x = " + x);
+	if (x < 5) k(null, x + 1);
+};
+var __test2 = function() {
+	callcc(function(cc) {
+		return callcc(function(cc) {
+			return 5;
+		});
+	}, null, function(e0, v0) {
+		callcc(function(cc) {
+			return 5;
+		}, null, function(e1, v1) {
+			var a = v0 + v1;
+			print("a = " + a);
+			var b = callcc(function(cc) {
+				var f = 6;
+				cc(null, f);
+			});
+			print("b = " + b);
+			var k;
+			var x = callcc(function(cc) {
+				k = cc;
+				return 0;
+			});
+			print("x = " + x);
+			if (x < 5) k(null, x + 1);
+		});
+	});
+};
+let t = use_callcc(_test2);
+let print = console.log;
+t();
